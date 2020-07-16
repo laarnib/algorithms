eabdb81adebaaf45ad3fdd1c2bba73a64c63b589
bacon <laarnijb@gmail.com>
2020-07-16 17:59:00 -0400

    Added selection, insertion, and merge sort algorithms. Deleted swap function.

--- c/algorithms/sort.c
+++ c/algorithms/sort.c
@@ -3,18 +3,39 @@
 
 int main (void)
 {
-    int arrsize, bubbleSteps;
-    int numbersForBubbleSort[8] = {6,3,8,5,2,7,4,1};
+    int arrsize, bubbleSteps, selectionSteps, insertionSteps;
+    //int numbersForBubbleSort[8] = {6,3,8,5,2,7,4,1};
+    int numbersForBubbleSort[8] = {1,2,3,4,5,6,7,8};
+    //int numbersForBubbleSort[8] = {8,7,6,5,4,3,2,1};
     //int numbersForSelectionSort[8] = {6,3,8,5,2,7,4,1};
+    int numbersForSelectionSort[8] = {1,2,3,4,5,6,7,8};
+    //int numbersForSelectionSort[8] = {8,7,6,5,4,3,2,1};
     //int numbersForInsertionSort[8] = {6,3,8,5,2,7,4,1};
+    int numbersForInsertionSort[8] = {1,2,3,4,5,6,7,8};
+    //int numbersForInsertionSort[8] = {8,7,6,5,4,3,2,1};
     //int numbersForMergeSort[8] = {6,3,8,5,2,7,4,1};
 
     arrsize = 8;
+    printf("Unsorted\n");
+    printSortedArray(numbersForInsertionSort, arrsize);
+    printf("\n\n");
 
-    // Using Bubble Sort
+    /// Using Bubble Sort
     bubbleSteps = bubbleSort(numbersForBubbleSort, arrsize);
     printSortedArray(numbersForBubbleSort, arrsize);
     printf("Bubble Sort (# of steps) : %i\n\n", bubbleSteps);
+
+    // Selection Sort
+    selectionSteps = selectionSort(numbersForSelectionSort, arrsize);
+    printSortedArray(numbersForSelectionSort, arrsize);
+    printf("Selection Sort (# of steps) : %i\n\n", selectionSteps);
+
+    // Insertion Sort
+    insertionSteps = insertionSort(numbersForInsertionSort, arrsize);
+    printf("\nSorted\n");
+    printSortedArray(numbersForInsertionSort, arrsize);
+    printf("Insertion Sort (# of steps) : %i\n\n", insertionSteps);
+
 
     return 0;
 }
--- c/algorithms/sortalgo.h
+++ c/algorithms/sortalgo.h
@@ -1,13 +1,10 @@
 void printSortedArray(int num[], int size);
-void swap(int array[], int index);
+int temp, step, swapCounter;
 
 // Bubble Sort Algorithm
 int bubbleSort(int numbers[], int arrsize)
 {
-    int swapCounter, step;
-
-    /*Set the swap counter to a non zero value so the program
-       can enter the while loop */
+    //Set the swap counter to a non zero value
     swapCounter = -1;
 
     // Track number of steps it takes to sort the numbers
@@ -24,31 +21,103 @@ int bubbleSort(int numbers[], int arrsize)
         for (int i = 0; i < arrsize - 1; i++)
         {
             step++;
-            if (numbers[i] > numbers[i+1])
+            if (numbers[i] > numbers[i + 1])
             {   
-                swap(numbers, i);
+                temp = numbers[i];
+                numbers[i] = numbers[i + 1];
+                numbers[i + 1] = temp;
                 swapCounter++;
             }
         }
     }
-    
+    return step;
+} 
+
+// Selection Sort
+int selectionSort (int numbers[], int size)
+{
+    int min, minIndex;
+
+    step = 0;
+    for (int i = 0; i < size; i++)
+    {
+        step++;
+        // Initialize the min value to the first element of the list
+        min  = numbers[i];
+        minIndex = i;
+
+        for (int j = i + 1; j < size; j++)
+        {
+            step++;
+            if (min > numbers[j])
+            {
+                min = numbers[j];
+                minIndex = j;
+            }
+        }
+        // Swap minimum number to the first unsorted element
+        temp = numbers[i];
+        numbers[i] = min;
+        numbers[minIndex] = temp;
+    }
+
     return step;
 }
 
-// Swap two numbers
-void swap(int array[], int index)
+// Insertion Sort
+int insertionSort(int numbers[], int size)
 {
-    int temp;
+    int value, indexOfValue;
+    
+    step = 0;
+    for (int i = 1; i < size; i++)
+    {
+        value = numbers[i];
+        indexOfValue = i;
 
-    temp = * (array + index);
-    * (array + index) = * (array + (index + 1));
-    * (array + (index + 1)) = temp;
+        for (int j = i - 1; j >= 0; j--)
+        {
+            step++;
+            if (value < numbers[j])
+            {
+                numbers[indexOfValue] = numbers[j];
+                numbers[j] = value;
+                indexOfValue--;
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+        
+    }
+    return step;
 }
+
+void mergeSort(int numbers, int left, int right)
+{
+    if (left < right)
+    {
+        // Calculate the mid
+        int mid = (left + right) / 2;
+        mergeSort(numbers, left, mid);  // merge sort the left portion of the array which starts at 0 and ends at mid;
+        mergeSort(numbers, mid + 1, right); // merge sort the right portion of the array
+        merge(numbers, left, mid, right);
+    }
+}
+
+/*// Merge the two sorted portions of the array
+void merge(int numbers[], int left, int mid, int right)
+{
+    int i, j, k;
+    int temp[right];  // Declare another array to store the sorted numbers
+}*/
 
 // Print sorted array
 void printSortedArray(int num[], int size)
 {
-    printf("Sorted Array : ");
+    printf("Array : ");
     for (int i = 0; i < size; i++)
     {
         printf("%i ", num[i]);
@@ -56,4 +125,7 @@ void printSortedArray(int num[], int size)
 
     printf("\n");
 }
+
+
+
 
